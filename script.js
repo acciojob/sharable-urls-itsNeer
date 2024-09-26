a336ae17f049911b50ef39cf3cb06a079cb65d54
Neeraj Shrivas <shrivasneeraj01@gmail.com>
2024-09-26 23:06:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // your code here
+ document.getElementById("button").onclick = function() {
+            const name = document.getElementById("name").value;
+            const year = document.getElementById("year").value;
+            const baseUrl = "https://localhost:8080/";
+            const queryString = `?name=${encodeURIComponent(name)}&year=${encodeURIComponent(year)}`;
+            const fullUrl = baseUrl + queryString;
+
+            document.getElementById("url").innerText = fullUrl;
+        };
